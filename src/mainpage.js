32964c6b01726e72dc49c7401c79d1363158e87e
Tran Hue Chi <tranhuechi1995@gmail.com>
2020-09-05 18:55:20 +0800

    Clean-up the code with better naming

--- src/mainpage.js
+++ src/mainpage.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
 
 const Mainpage = () => {
-    let [id, setId] = useState(1);
+    let [idForAddItem, setIdForAddItem] = useState(1);
     let [todoList, setTodoList] = useState([]);
-    let [todo, setTodo] = useState('');
-    let [IdforDel, setIdforDel] = useState(-1);
+    let [todoItem, setTodoItem] = useState('');
+    let [idForDel, setIdForDel] = useState(-1);
     let [toUpdateContent, setToUpdateContent] = useState('');
-    let [IdforUpdate, setIdforUpdate] = useState(-1);
+    let [idForUpdate, setIdForUpdate] = useState(-1);
 
     // get the initial state
     useEffect(() => {
@@ -21,10 +21,10 @@ const Mainpage = () => {
                 newList.push({todoId: item._id, todoContent: item.toAdd});
                 maxId = Math.max(maxId, parseInt(item._id));
             }
-            setId(maxId + 1);
+            setIdForAddItem(maxId + 1);
             console.log(newList[0].todoId);
-            setIdforDel(newList[0].todoId);
-            setIdforUpdate(newList[0].todoId);
+            setIdForDel(newList[0].todoId);
+            setIdForUpdate(newList[0].todoId);
             setTodoList(newList);
         })
         .catch(err => console.log(err))
@@ -45,56 +45,54 @@ const Mainpage = () => {
         .catch(err => console.log(err));
     }
 
-    const addHandler = (e) => {
+    const addHandler = (addEvent) => {
         // send a post request to the server from here!
-        e.preventDefault();
-        if (todo === '') {
+        addEvent.preventDefault();
+        if (todoItem === '') {
             alert("You forget to input sth!");
         } else {
             // let currentId = Math.floor(Math.random() * 100000);
-            setId(id + 1); // increase the id
-            let requestBody = { _id: id, toAdd: todo };
+            setIdForAddItem(idForAddItem + 1); // increase the id
+            let requestBody = { _id: idForAddItem, toAdd: todoItem };
             sendRequest('POST', requestBody);
-            setTodoList(currentTodo => [...currentTodo, {todoId: id, todoContent: todo}]);
-            setIdforDel(id);
-            setIdforUpdate(id);
-            setTodo('');
+            setTodoList(currentTodo => [...currentTodo, {todoId: idForAddItem, todoContent: todoItem}]);
+            setIdForDel(idForAddItem);
+            setIdForUpdate(idForAddItem);
+            setTodoItem('');
         }
     };
 
-    const updateHandler = (e) => {
-        e.preventDefault();
+    const updateHandler = (updateEvent) => {
+        updateEvent.preventDefault();
         if (toUpdateContent === '') {
             alert("You forget update content!");
         } else {
-            if (IdforUpdate === -1) {
+            if (idForUpdate === -1) {
                 alert("You forget to choose an item to update!");
             } else {
                 // do the update here!
-                console.log(todoList[0].todoId);
-                console.log(IdforUpdate);
-                let itemToUpdate = todoList.find(singleTodo => singleTodo.todoId === parseInt(IdforUpdate));
+                let itemToUpdate = todoList.find(singleTodo => singleTodo.todoId === parseInt(idForUpdate));
                 if (window.confirm(`You want to update "${itemToUpdate.todoContent}" with "${toUpdateContent}" ?`)) {
-                    const newTodoList = todoList.map(singletodo => singletodo.todoId == IdforUpdate
+                    const newTodoList = todoList.map(singletodo => singletodo.todoId === parseInt(idForUpdate)
                     ? {...singletodo, todoContent: toUpdateContent}
                     : singletodo);
                     // send a put request here
-                    let requestBody = { itemToUpdateId: IdforUpdate, itemToUpdateContent: toUpdateContent };
+                    let requestBody = { itemToUpdateId: idForUpdate, itemToUpdateContent: toUpdateContent };
                     sendRequest('PUT', requestBody);
                     setTodoList(newTodoList);
-                    setIdforUpdate(-1);
+                    setIdForUpdate(-1);
                     setToUpdateContent('');
                 }
             }
         }
     }
 
-    const delHandler = (e) => {
+    const delHandler = (deleteEvent) => {
         // send a delete request to the server
-        e.preventDefault(); // Do not forget this!
+        deleteEvent.preventDefault(); // Do not forget this!
         let content = '';
         for (let i = 0; i < todoList.length; i++) {
-            if (todoList[i].todoId === parseInt(IdforDel)) {
+            if (todoList[i].todoId === parseInt(idForDel)) {
                 content = todoList[i].todoContent;
                 break;
             }
@@ -104,22 +102,23 @@ const Mainpage = () => {
         } else {
             if (content !== undefined && window.confirm(`Do you want to delete "${content}"`)) {
                 setTodoList(todoList.filter(singleTodo => {
-                    return singleTodo.todoId !== parseInt(IdforDel);
+                    return singleTodo.todoId !== parseInt(idForDel);
                 }));
-                let requestBody = { itemToDelete: IdforDel };
+                let requestBody = { itemToDelete: idForDel };
                 sendRequest('DELETE', requestBody);
-                setIdforDel(-1);
+                setIdForDel(-1);
             }
         } 
     };
 
-    const delWholeHandler = () => {
+    const delWholeHandler = (deleteWholeEvent) => {
+        deleteWholeEvent.preventDefault();
         // send a delete request to the server
         if (todoList.length > 0) {
             if (window.confirm("Do you want to delete the whole list?")) {
                 setTodoList([]);
                 // send a delete request
-                setIdforDel(-1);
+                setIdForDel(-1);
                 let requestBody = { itemToDelete: "Whole list"};
                 sendRequest('DELETE', requestBody);
             }
@@ -140,15 +139,15 @@ const Mainpage = () => {
             }
             <form className="add-item-container" onSubmit={addHandler}>
                 <label>To add item
-                    <input type="text" placeholder="Add your todo" value={todo} 
-                        onChange={e => setTodo(e.target.value)} />
+                    <input type="text" placeholder="Add your todo" value={todoItem} 
+                        onChange={event => setTodoItem(event.target.value)} />
                     <input type="submit" value="ADD"/>
                 </label>
             </form>
 
             <form onSubmit={updateHandler}>
                 <label>To update item
-                    <select value={IdforUpdate} onChange={e => setIdforUpdate(e.target.value)}>
+                    <select value={idForUpdate} onChange={event => setIdForUpdate(event.target.value)}>
                         <option value='0' disabled>Select an item</option>
                         {todoList.map(singleTodo => {
                             return (
@@ -157,14 +156,14 @@ const Mainpage = () => {
                         })}
                     </select>
                     <input type="text" placeholder="Change item to" value={toUpdateContent} 
-                        onChange={e => setToUpdateContent(e.target.value)}/>
+                        onChange={event => setToUpdateContent(event.target.value)}/>
                     <input type="submit" value="UPDATE" />
                 </label>
             </form>
 
             <form className="delete-item-container" onSubmit={delHandler}>
                 <label>To delete items
-                    <select value={IdforDel} onChange={e => setIdforDel(e.target.value)}>
+                    <select value={idForDel} onChange={event => setIdForDel(event.target.value)}>
                         <option value='0' disabled>Select an item</option>
                         {todoList.map(singleTodo => {
                             return (
